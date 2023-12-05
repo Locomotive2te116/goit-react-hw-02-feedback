e3c849e94ce0213020e41b64c0cc3ef0b0267fe7
Locomotive2te116 <locomotive2te116@gmail.com>
2023-12-05 18:14:28 +0200

    start

---
+++ src/components/feedback/feedback.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const feedbackData = () => {
+  return (
+    <>
+      <div>
+        <p>Please leave feedback</p>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+      </div>
+    </>
+  );
+};
